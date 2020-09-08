c0a2ac3cce838daf4a41742ae9fec5b846ef7310
i-Quicker <sun905817@163.com>
2020-09-08 11:00:25 +0800

    v9.8

---
+++ WaterCloud.Service/OperationMonitoring/PMHomeDepartmentQualityStatisticsService.cs
@@ -0,0 +1,20 @@
+﻿using Chloe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WaterCloud.Domain.Entity.OperationMonitoring;
+
+namespace WaterCloud.Service.OperationMonitoring
+{
+    public class PMHomeDepartmentQualityStatisticsService : DataFilterService<PMHomeDepartmentQualityStatisticsEntity>, IDenpendency
+    {
+        public PMHomeDepartmentQualityStatisticsService(IDbContext context) : base(context)
+        {
+        }
+        public async Task<List<PMHomeDepartmentQualityStatisticsEntity>> GetList()
+        {
+            return repository.IQueryable().ToList();
+        }
+    }
+}
--- WaterCloud.Web/Areas/ProductionManage/Controllers/ProductionManageHomeController.cs
+++ WaterCloud.Web/Areas/ProductionManage/Controllers/ProductionManageHomeController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Serenity;
+using WaterCloud.Service.OperationMonitoring;
 
 namespace WaterCloud.Web.Areas.ProductionManage.Controllers
 {
     [Area("ProductionManage")]
     public class ProductionManageHomeController : ControllerBase
     {
+        public PMHomeDepartmentQualityStatisticsService _pdqsService { get; set; }
 
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetDataDepartmentQualityStatistics()
+        {
+            var data = await _pdqsService.GetList();
+            data = data.Where(p => p.IsEffective == 1).ToList();
+            return Content(data.ToJson());
+        }
     }
 }
