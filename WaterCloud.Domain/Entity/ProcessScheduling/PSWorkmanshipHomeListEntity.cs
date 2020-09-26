700eab1b2cd6a9aba6422e458ed339fca0941c50
i-Quicker <sun905817@163.com>
2020-09-26 13:09:56 +0800

    v9.26

---
+++ WaterCloud.Domain/Entity/ProcessScheduling/PSWorkmanshipHomeListEntity.cs
@@ -0,0 +1,33 @@
+﻿using Chloe.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterCloud.Domain.Entity.ProcessScheduling
+{
+    [TableAttribute("Sys_PSWorkmanshipHomeList")]
+    public class PSWorkmanshipHomeListEntity : IEntity<PSWorkmanshipHomeListEntity>
+    {
+        [ColumnAttribute("id", IsPrimaryKey = true)]
+        public string id { get; set; }
+        public string MoldNo { get; set; }
+        public string TENo { get; set; }
+        public string MoldType { get; set; }
+        public string ModelProtection { get; set; }
+        public string MoldState { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? TestDate { get; set; }
+        public string PartNumber { get; set; }
+        public string PartName { get; set; }
+        public string Describe { get; set; }
+        public string Type { get; set; }
+        public int? Number { get; set; }
+        public string MaterialScience { get; set; }
+        public string Hardness { get; set; }
+        public string Specifications { get; set; }
+        public string Formwork { get; set; }
+        public string ProductionDeliveryDate { get; set; }
+        public DateTime? CreationTime { get; set; }
+        public int? IsEffective { get; set; }
+    }
+}
---
+++ WaterCloud.Web/Areas/ProcessScheduling/Controllers/WorkmanshipHomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaterCloud.Code;
+using WaterCloud.Service.ProcessScheduling;
+
+namespace WaterCloud.Web.Areas.ProcessScheduling.Controllers
+{
+    [Area("ProcessScheduling")]
+    public class WorkmanshipHomeController : ControllerBase
+    {
+        public PSWorkmanshipHomeListService _whlService { get; set; }
+        
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetGridJson(Pagination pagination)
+        {
+            pagination.order = "desc";
+            pagination.sort = "MoldNo desc";
+            var data = await _whlService.GetList(pagination);
+            return Success(pagination.records, data);
+        }
+    }
+}
