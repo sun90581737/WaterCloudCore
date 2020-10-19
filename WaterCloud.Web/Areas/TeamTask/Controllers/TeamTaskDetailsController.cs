5f7eecec02e3e37f79ee309b89e1e659391c65b4
i-Quicker <sun905817@163.com>
2020-10-19 14:53:11 +0800

    v10.19

---
+++ WaterCloud.Web/Areas/TeamTask/Controllers/TeamTaskDetailsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaterCloud.Code;
+using WaterCloud.Service.TeamTask;
+
+namespace WaterCloud.Web.Areas.TeamTask.Controllers
+{
+    [Area("TeamTask")]
+    public class TeamTaskDetailsController : ControllerBase
+    {
+        public TeamTaskDetailsListService _ttdlService { get; set; }
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetGridJson(Pagination pagination)
+        {
+            pagination.order = "desc";
+            pagination.sort = "MoldNo desc";
+            var data = await _ttdlService.GetList(pagination);
+            return Success(pagination.records, data);
+        }
+    }
+}
