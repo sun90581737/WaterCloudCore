6929a22e4bbdecc275cb38bf30691210ea5ec7fd
i-Quicker <sun905817@163.com>
2020-10-23 10:02:30 +0800

    v10.23

---
+++ WaterCloud.Service/ProcessScheduling/PSProcessDetailsListService.cs
@@ -0,0 +1,20 @@
+﻿using Chloe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WaterCloud.Domain.Entity.ProcessScheduling;
+
+namespace WaterCloud.Service.ProcessScheduling
+{
+    public class PSProcessDetailsListService : DataFilterService<PSProcessDetailsListEntity>, IDenpendency
+    {
+        public PSProcessDetailsListService(IDbContext context) : base(context)
+        {
+        }
+        public async Task<List<PSProcessDetailsListEntity>> GetList()
+        {
+            return repository.IQueryable().ToList();
+        }
+    }
+}
---
+++ WaterCloud.Web/Areas/ProcessScheduling/Controllers/ProcessDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serenity;
+using WaterCloud.Service.ProcessScheduling;
+
+namespace WaterCloud.Web.Areas.ProcessScheduling.Controllers
+{
+    [Area("ProcessScheduling")]
+    public class ProcessDetailsController : ControllerBase
+    {
+        public PSProcessDetailsListService _pdlService { get; set; }
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetPSProcessDetailsList()
+        {
+            var data = await _pdlService.GetList();
+            data = data.Where(p => p.IsEffective == 1).ToList();
+            return Content(data.ToJson());
+        }
+    }
+}
