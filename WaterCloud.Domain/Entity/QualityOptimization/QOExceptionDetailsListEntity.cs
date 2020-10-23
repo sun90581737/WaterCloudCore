badbe8cd326b341587a206b2d3fe666e3ce999c4
i-Quicker <sun905817@163.com>
2020-10-23 16:25:54 +0800

    v10.23

---
+++ WaterCloud.Domain/Entity/QualityOptimization/QOExceptionDetailsListEntity.cs
@@ -0,0 +1,24 @@
+﻿using Chloe.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterCloud.Domain.Entity.QualityOptimization
+{
+    [TableAttribute("Sys_QOExceptionDetailsList")]
+    public class QOExceptionDetailsListEntity : IEntity<QOExceptionDetailsListEntity>
+    {
+        [ColumnAttribute("id", IsPrimaryKey = true)]
+        public int? id { get; set; }
+        public string ExceptionNumber { get; set; }
+        public string ExceptionModule { get; set; }
+        public string FaultDescription { get; set; }
+        public string Department { get; set; }
+        public string ResponsibleProcess { get; set; }
+        public string PersonLiable { get; set; }
+        public string ResponsibleSupplier { get; set; }
+        public string AnomalyTracking { get; set; }
+        public DateTime? CreationTime { get; set; }
+        public int? IsEffective { get; set; }
+    }
+}
---
+++ WaterCloud.Web/Areas/QualityOptimization/Controllers/ExceptionDetailsListController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaterCloud.Code;
+using WaterCloud.Service.QualityOptimization;
+
+namespace WaterCloud.Web.Areas.QualityOptimization.Controllers
+{
+    [Area("QualityOptimization")]
+    public class ExceptionDetailsListController : ControllerBase
+    {
+        public QOExceptionDetailsListService _edlService { get; set; }
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetGridJson(Pagination pagination)
+        {
+            pagination.order = "desc";
+            pagination.sort = "ExceptionNumber desc";
+            var data = await _edlService.GetList(pagination);
+            return Success(pagination.records, data);
+        }
+    }
+}
