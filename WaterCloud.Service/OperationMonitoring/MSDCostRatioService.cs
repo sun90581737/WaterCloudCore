cd09e5db56ccfff41929a2c32e0b11b6de508a02
i-Quicker <sun905817@163.com>
2020-10-28 12:41:11 +0800

    v10.28

---
+++ WaterCloud.Service/OperationMonitoring/MSDCostRatioService.cs
@@ -0,0 +1,20 @@
+﻿using Chloe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WaterCloud.Domain.Entity.OperationMonitoring;
+
+namespace WaterCloud.Service.OperationMonitoring
+{
+    public class MSDCostRatioService : DataFilterService<MSDCostRatioEntity>, IDenpendency
+    {
+        public MSDCostRatioService(IDbContext context) : base(context)
+        {
+        }
+        public async Task<List<MSDCostRatioEntity>> GetList()
+        {
+            return repository.IQueryable().ToList();
+        }
+    }
+}
---
+++ WaterCloud.Web/Areas/OperationMonitoring/Controllers/MoldScheduleDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serenity;
+using WaterCloud.Service.OperationMonitoring;
+
+namespace WaterCloud.Web.Areas.OperationMonitoring.Controllers
+{
+    [Area("OperationMonitoring")]
+    public class MoldScheduleDetailsController : ControllerBase
+    {
+        public MSDCostRatioService _crService { get; set; }
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetDataMSDCostRatio()
+        {
+            var data = await _crService.GetList();
+            data = data.Where(p => p.IsEffective == 1).ToList();
+            return Content(data.ToJson());
+        }
+    }
+}
