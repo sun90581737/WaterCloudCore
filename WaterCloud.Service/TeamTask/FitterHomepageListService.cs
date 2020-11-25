5d1dd63ecd20dd62778a8c25cf77d8c2e0ac7871
i-Quicker <sun905817@163.com>
2020-11-25 09:55:20 +0800

    v11.25

--- WaterCloud.Service/TeamTask/FitterHomepageListService.cs
+++ WaterCloud.Service/TeamTask/FitterHomepageListService.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Chloe;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WaterCloud.Code;
+using WaterCloud.Domain.Entity.TeamTask;
 
 namespace WaterCloud.Service.TeamTask
 {
-    public class FitterHomepageListService
+    public class FitterHomepageListService : DataFilterService<FitterHomepageListEntity>, IDenpendency
     {
+        public FitterHomepageListService(IDbContext context) : base(context)
+        {
+        }
+        public async Task<List<FitterHomepageListEntity>> GetList(Pagination pagination, string keyvalue)
+        {
+            ////获取数据权限
+            //var list = GetDataPrivilege("u", className.Substring(0, className.Length - 7));
+
+            //list = list.Where(t => t.IsEffective == 1);
+            //return GetFieldsFilterData(await repository.OrderList(list, pagination), className.Substring(0, className.Length - 7));
+
+            var list = repository.IQueryable();
+            if (!string.IsNullOrEmpty(keyvalue))
+            {
+                DateTime startTime = keyvalue.Substring(0, 10).ToDate();
+                DateTime endTime = keyvalue.Remove(0, 13).ToDate().AddDays(0);
+
+                list = list.Where(t => t.PlannedDeliveryDate >= startTime && t.PlannedDeliveryDate <= endTime);
+            }
+            list = list.Where(t => t.IsEffective == 1);
+            return await repository.OrderList(list, pagination);
+        }
     }
 }
---
+++ WaterCloud.Web/Areas/TeamTask/Controllers/FitterHomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterCloud.Code;
+using WaterCloud.Service.TeamTask;
+
+namespace WaterCloud.Web.Areas.TeamTask.Controllers
+{
+    [Area("TeamTask")]
+    public class FitterHomeController : ControllerBase
+    {
+        public FitterHomepageListService _fhlService { get; set; }
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public async Task<ActionResult> GetGridJson(Pagination pagination, string keyvalue)
+        {
+            pagination.order = "desc";
+            pagination.sort = "ProductName ASC";
+            var data = await _fhlService.GetList(pagination, keyvalue);
+            return Success(pagination.records, data);
+        }
+    }
+}
